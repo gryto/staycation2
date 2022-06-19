cf6f2eeb53d38f22bea41141235346b350bf85a5
solihahnur <57170706+gryto@users.noreply.github.com>
2022-06-19 16:57:59 +0700

    ypdate depoy_script folder
    Signed-off-by: solihahnur <57170706+gryto@users.noreply.github.com>

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,9 +1,13 @@
 # add nodejs to yum
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash 
+. ~/.nvm/nvm.sh
 yum install nodejs -y #default-jre ImageMagick
 
-# install pm2 module globaly
-npm install -g n
-
-# delete existing bundle
-cd /home/ec2-user
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/staycation2"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
