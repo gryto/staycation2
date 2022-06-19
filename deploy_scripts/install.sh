4bd3874b3a278ada66b4feb97f7c9da4c5028cab
solihahnur <57170706+gryto@users.noreply.github.com>
2022-06-19 16:27:59 +0700

    install.sh updated
    Signed-off-by: solihahnur <57170706+gryto@users.noreply.github.com>

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -3,9 +3,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
 yum install nodejs -y #default-jre ImageMagick
 
 # install pm2 module globaly
-npm install -g pm2
-pm2 update
+npm install -g n
 
 # delete existing bundle
 cd /home/ec2-user
-rm -rf node
