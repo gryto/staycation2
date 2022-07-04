727315439333ed3ee22a57a0df91ec47d35e8542
solihahnur <57170706+gryto@users.noreply.github.com>
2022-07-04 22:26:38 +0700

    fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import { ToastContainer} from "react-toastify";
-//import LandingPage from "pages/LandingPage";
+import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
 import Checkout from "pages/Checkout";
 import Example from "pages/Example";
